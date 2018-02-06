10ab0845a8b8e281e9f5183f4fe9f8452018afaa
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-06 16:30:43 +0000

    close #1 signup form

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,16 @@ require 'sinatra/base'
 
 class Nodo < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
+    @user_name = session[:user_name]
     erb :'users/index'
   end
 
-
+  post '/user' do
+    session[:user_name] = params[:user_name]
+    redirect to '/'
+  end
 
 end
