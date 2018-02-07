72d82dc52686d0db507ab06ca927ac5af3ab5b38
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-07 11:07:26 +0000

    Add password field

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -2,6 +2,7 @@ feature 'user sign up' do
   scenario 'user enter their name' do
     visit('/')
     fill_in 'user_name', with: 'Fran'
+    fill_in 'password', with: 'Idunno'
     click_button 'Submit'
     expect(page).to have_text('Fran')
   end
