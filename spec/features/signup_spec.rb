5e15748974a2963e01ba4e8ad424d7129ef3f50f
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-07 11:31:27 +0000

    user signup is sotred in the database

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -7,3 +7,12 @@ feature 'user sign up' do
     expect(page).to have_text('Fran')
   end
 end
+
+feature 'store user in database' do
+  scenario 'enter password' do
+    visit('/')
+    fill_in 'user_name', with: 'Fran'
+    fill_in 'password', with: 'Idunno'
+    expect { click_button('Submit') }.to change(User, :count).by 1
+  end
+end
