c7d6f32aa7a1b1d485918a36ca5ff0fa94b6e8e6
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-08 14:28:13 +0000

    fix login auth issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,11 @@ class User
 
   def self.authenticate(username, password)
     auth_user = first(username: username)
-    p "auth user???"
-    p auth_user
-    auth_user.password == password ? auth_user : nil
+    if auth_user.nil?
+      nil
+    else
+      auth_user.password == password ? auth_user : nil
+    end
   end
+
 end
