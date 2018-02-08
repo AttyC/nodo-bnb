e585fb63db793d624840784caff998916ae75011
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-08 12:02:55 +0000

    table for list multiple spaces

--- spec/features/list_multiple_spaces_spec.rb
+++ spec/features/list_multiple_spaces_spec.rb
@@ -2,15 +2,19 @@ feature 'Listing multiple spaces' do
   scenario 'A user can list multiple spaces' do
     fill_in_listing
     click_button 'List my space'
-    fill_in_listing(name: 'Fran',
+    fill_in_listing(name: 'Anything',
                     description: 'The best place!',
                     price: '10',
                     from_date: '16/03/2018',
                     to_date: '16/04/2018')
     click_button 'List my space'
+    expect(page).to have_text('Fran')
     expect(page).to have_text('Nice place!')
+    expect(page).to have_text('100')
     expect(page).to have_text('2018-10-12')
+    expect(page).to have_text('Anything')
     expect(page).to have_text('The best place!')
+    expect(page).to have_text('10')
     expect(page).to have_text('2018-03-16')
   end
 end
