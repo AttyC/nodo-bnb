f7a55962eff6633988d0cb66ec64a43138bb6ff9
Tagrand <thomas.a.grand@gmail.com>
2018-02-08 12:29:23 +0000

    check one data avalible is listed

--- spec/features/list_space_spec.rb
+++ spec/features/list_space_spec.rb
@@ -20,6 +20,11 @@ feature 'View new space' do
     expect(page).to have_text('Fran')
     expect(page).to have_text('Nice place!')
     expect(page).to have_text('100')
-    expect(page).to have_text('12-10-2018')
+  end
+
+  scenario 'see\'s first date avaliable' do
+     fill_in_listing
+     click_button 'List my space'
+     expect(page).to have_text('12-10-2018')
   end
 end
